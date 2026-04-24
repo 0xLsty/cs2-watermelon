8b0a660ac95c3090ecb95f7bf4a50672df750578
0xLsty <217672252+0xLsty@users.noreply.github.com>
2026-04-24 01:43:04 -0300

    correction

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,22 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  storeGet: (key) => ipcRenderer.invoke('store:get', key),
-  storeSet: (key, value) => ipcRenderer.invoke('store:set', key, value),
-  storeDelete: (key) => ipcRenderer.invoke('store:delete', key),
+const ALLOWED_KEYS = new Set(['portfolio', 'historico', 'schemaVersion']);
+
+function assertKey(key) {
+  if (!ALLOWED_KEYS.has(key)) throw new Error(`Chave inválida: ${key}`);
+}
+
+contextBridge.exposeInMainWorld('api', {
+  storeGet: (key) => {
+    assertKey(key);
+    return ipcRenderer.invoke('store:get', key);
+  },
+  storeSet: (key, value) => {
+    assertKey(key);
+    return ipcRenderer.invoke('store:set', key, value);
+  },
+  storeDelete: (key) => {
+    assertKey(key);
+    return ipcRenderer.invoke('store:delete', key);
+  },
 });
